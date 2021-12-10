353b1a04c35ca909a313b90a19a84863d14bafae
O-Almeida <brunof.santos.almeida@gmail.com>
2021-12-10 15:58:41 +0000

    changed the way to generate random XY

--- food.py
+++ food.py
@@ -1,6 +1,10 @@
 from turtle import Turtle
 import random
 
+MAX_VALUE = 340
+possible_xy = [x for x in range(-MAX_VALUE, MAX_VALUE, 20)]
+
+
 class Food(Turtle):
     def __init__(self):
         super().__init__()
@@ -9,4 +13,5 @@ class Food(Turtle):
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
-        self.goto(random.randint(-330, 330), random.randint(-330, 330))
+        self.goto(possible_xy[random.randint(0, len(possible_xy)-1)], possible_xy[random.randint(0, len(possible_xy)-1)])
+
