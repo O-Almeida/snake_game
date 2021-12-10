c0d876c6f99048e0665fd179bf2878b6bff47d74
O-Almeida <brunof.santos.almeida@gmail.com>
2021-12-10 16:52:45 +0000

    minor change scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 18, "normal")
 
 
 class Scoreboard(Turtle):
@@ -9,9 +11,12 @@ class Scoreboard(Turtle):
         self.penup()
         self.color("white")
         self.goto(0, 320)
-        self.write(f"Score = {self.score}", False, "center", ("Arial", 18, "normal"))
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
         self.clear()
-        self.write(f"Score = {self.score}", False, "center", ("Arial", 18, "normal"))
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score = {self.score}", False, ALIGNMENT, FONT)
